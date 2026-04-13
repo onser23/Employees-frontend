ebfbd48289f59e98f3ae9e4e6e854069813891ee
onser23 <semed.semedzade@yahoo.com>
2026-04-13 16:45:21 +0400

    new

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+// const API_URL = "http://localhost:5000/api";
+const API_URL = "https://employees-backend-nu.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
