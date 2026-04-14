637180c063a3fd10ecdc318f11567eb217be837a
onser23 <semed.semedzade@yahoo.com>
2026-04-14 11:28:56 +0400

    logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
           <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-purple-600 rounded-full flex items-center justify-center mx-auto mb-4 shadow-lg">
             {/* <Shield className="w-10 h-10 text-white" />
              */}
-            <img src={logo} alt="Logo" />
+            <img width="100" height="50" src={logo} alt="Logo" />
           </div>
           <h1 className="text-3xl font-bold text-gray-800 mb-2">Admin Panel</h1>
           <p className="text-gray-500">İşçi İdarəetmə Sistemi</p>
