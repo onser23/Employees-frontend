b2bca34229f13e7376c45d693627df394b8b3ee4
onser23 <semed.semedzade@yahoo.com>
2026-05-19 10:13:14 +0400

    New

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { Shield, User, Lock, Eye, EyeOff } from "lucide-react";
 import toast, { Toaster } from "react-hot-toast";
+import SVGComponent from "../components/svgviewer-react-output";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -93,7 +94,8 @@ const Login = () => {
         {/* Logo və Başlıq */}
         <div className="text-center mb-8">
           <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-purple-600 rounded-full flex items-center justify-center mx-auto mb-4 shadow-lg">
-            <Shield className="w-10 h-10 text-white" />
+            {/* <Shield className="w-10 h-10 text-white" /> */}
+            <SVGComponent />
           </div>
           <h1 className="text-3xl font-bold text-gray-800 mb-2">Admin Panel</h1>
           <p className="text-gray-500">İşçi İdarəetmə Sistemi</p>
